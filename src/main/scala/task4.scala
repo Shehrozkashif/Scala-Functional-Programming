91ffae12a83b96d5faf70be04b8fde9485d88688
Shehrozkashif <sharooz57@gmail.com>
2023-10-27 22:27:23 +0500

    Assignment 1

--- src/main/scala/task4.scala
+++ src/main/scala/task4.scala
@@ -10,9 +10,32 @@ object task4 {
     }
     println("The number of even numbers in the array: " + e)
   }
+  def countEvenRecursively(arr: Array[Int]): Int = {
+    if (arr.isEmpty) {
+      0
+    } else {
+      val currentElement = arr.head
+      val restOfArray = arr.tail
+      val evenCountInRest = countEvenRecursively(restOfArray)
+
+      if (currentElement % 2 == 0) {
+        evenCountInRest + 1
+      } else {
+        evenCountInRest // Otherwise, keep the count as it is
+      }
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     val randomArray = Array.fill(100)(util.Random.nextInt(100) + 1)
     even(randomArray)
+
+    val evenCount1 = countEvenRecursively(randomArray)
+    println(s"Using recursion: Number of even values = $evenCount1")
+
+    // Technique 2: Using the count higher-order method
+    val evenCount2 = randomArray.count(_ % 2 == 0)
+    println(s"Using count method: Number of even values = $evenCount2")
+
   }
 }
--- src/main/scala/task5.scala
+++ src/main/scala/task5.scala
@@ -1,3 +1,19 @@
 object task5 {
+  def main(args: Array[String]): Unit = {
+    val lst = Array(1, 2, 3, 4, 5)
 
+    def func(x: Int): Boolean = x % 2 == 0 // Define a function that checks if an entry is even or not
+
+    val result = buildMap(lst, func)
+    println(result)
+  }
+
+  def buildMap[A, B](data: Seq[A], f: A => B): Map[B, A] = {
+    var resultMap = Map[B, A]()
+    for (element <- data) {
+      val key = f(element)
+      resultMap += (key -> element)
+    }
+    resultMap
+  }
 }
